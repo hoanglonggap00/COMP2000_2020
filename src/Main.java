0a86f6c3eecef86935f638588262bf83055df332
hoanglonggiap <nhatlinh161232@gmail.com>
2020-08-12 17:14:23 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        JFrame f = new JFrame();
+        
+        f.setSize(720,720);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
